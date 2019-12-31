918bf00ad43928f8a615f1e14bd6e581893a857b
Adrian Cheong <adriancheong@gic.com.sg>
2019-12-31 17:10:09 +0800

    1.2

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,4 @@ pwd
 echo $DOTNET_CLI_HOME
 export DOTNET_CLI_HOME=/tmp
 echo $DOTNET_CLI_HOME
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/
-pwd
 dotnet awsdevops.dll
