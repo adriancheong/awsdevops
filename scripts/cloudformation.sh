998292186138f5d69b1fe543d88f7cc4014e393e
Adrian Cheong <adriancheong@gic.com.sg>
2020-01-02 20:43:53 +0800

    1.8

--- scripts/cloudformation.sh
+++ scripts/cloudformation.sh
@@ -1,1 +1,3 @@
 aws cloudformation create-stack --stack-name awsdevopsstack --template-body file://cloudformation.template
+or
+aws cloudformation deploy --stack-name awsdevopsstack --template-file cloudformation.template
